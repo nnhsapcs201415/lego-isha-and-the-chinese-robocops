b6025605d5e62a06aa1d4b9d61e6489eb9ace39c
pdchao <PDChao@student.naperville203.org>
2017-05-10 14:17:32 -0500

    5/10/17
    finished travel test, have schmit test

--- programs/TravelTest.java
+++ programs/TravelTest.java
@@ -12,19 +12,19 @@ public class TravelTest {
   TouchSensor bump = new TouchSensor(SensorPort.S1);
 
   public void go() {
-    pilot.travel(20);
+    pilot.travel(500);
     pilot.rotate(90);
-    pilot.travel(20);
+    pilot.travel(500);
     pilot.rotate(90);
-    pilot.travel(20);
-    pilot.rotate(90);
-    pilot.travel(20);
-    pilot.rotate(90);
+    pilot.travel(500);
+    pilot.rotate(93);
+    pilot.travel(500);
+    pilot.rotate(95);
   }
 
   public static void main(String[] args) {
     TravelTest traveler = new TravelTest();
-    traveler.pilot = new DifferentialPilot(56, ,Motor.B, Motor.C);
+    traveler.pilot = new DifferentialPilot(56, 107,Motor.B, Motor.C);
         traveler.go();
       }
 }
