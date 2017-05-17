92e286739abe8202c2a92eff92ecb19c2719b282
pdchao <PDChao@student.naperville203.org>
2017-05-17 14:40:56 -0500

    5/17/17

--- programs/LightDetect.java
+++ programs/LightDetect.java
@@ -5,24 +5,26 @@ import lejos.nxt.*;
  */
 public class LightDetect implements SensorPortListener
 {
-    
     public static void main(String args[])
     {
         LightDetect detector = new LightDetect();        
         SensorPort.S3.addSensorPortListener(detector);
-        LightSensor light = new LightSensor(SensorPort.S3);        
         Button.ENTER.waitForPressAndRelease();
     }
     public void stateChanged( SensorPort aSource, int aOldValue, int aNewValue)
     {
-                
-        while( aNewValue < 600 )
+        LightSensor light = new LightSensor(SensorPort.S3);                  
+        if( light.getLightValue() > 40 )
         {
             Motor.B.forward();
             Motor.C.forward();
-            System.out.println(//getLightValue );
+            System.out.println(light.getLightValue());
         }
-        System.out.println("Stop Moving");
-        
+        else
+        {
+            Motor.B.stop();
+            Motor.C.stop();
+            System.out.println("Stop Moving:"+ light.getLightValue());
+        }
     }
 }
